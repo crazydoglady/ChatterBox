1b349e8147190b4642bb73e0058a90e31432b6f1
CrazyDogLady <valkharvey@gmail.com>
2015-02-05 12:42:08 -0500

    adding initial rough layout and js methods outline

---
+++ main.js
@@ -0,0 +1,29 @@
+var chats = {
+  init: function() {
+    chats.initStyling();
+    chats.initEvents();
+  },
+  initStyling: function() {
+
+  },
+  initEvents: function() {
+
+  },
+  createChat: function() {
+
+  },
+  renderChat: function(){
+    //set timeout to auto refresh page
+  };
+  
+
+};//end chats methods
+
+
+
+
+
+$(document).ready(function() {
+  chats.init();
+
+});//end doc ready
