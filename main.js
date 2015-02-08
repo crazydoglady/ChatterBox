21c104271325ee8d20db32adfa92c915bd8bf8cd
CrazyDogLady <valkharvey@gmail.com>
2015-02-07 21:17:06 -0500

    logout button now works and hides convo, clears userId from local storage.

--- main.js
+++ main.js
@@ -1,11 +1,6 @@
-
-
 $(document).ready(function() {
   chats.init();
   setInterval(chats.renderChat, 1000);
-
-    // to delete bad data uncomment out this line and put in correct id from server
-   //chatInTheBox1.deleteMessage('54d639b0ddad9b0300000011');
 
 });//end doc ready
 
@@ -18,15 +13,15 @@ var chats = {
   initStyling: function() {
     chats.renderChat();
   },
-  initEvents: function() {
 
+  initEvents: function() {
     $('.userId').on('submit', function(event){
       event.preventDefault();
       var userName = $('#userInput').val();
     localStorage.setItem( 'userId', userName);
       console.log(userName);
-      $('.userId').removeClass('show');
-    });
+
+    });//end submit userId
 
     //on click of send msg button trigger these events
     $('#create').on('submit', function(event){
@@ -40,19 +35,14 @@ var chats = {
 
     });//end submit event for .sendMessage button
 
-//   $('section').on('click', '.deleteMessage', function (event) {
-//   event.preventDefault();
-//   var taskid = $(this).closest('article').data('_id');
-//   console.log(taskid);
-//   tasks.deleteTask(taskid);
-// });
+    $('.logout').on('submit', function(event){
+      event.preventDefault();
+      console.log("logoutish1");
+      localStorage.removeItem('userId')
+      console.log("logoutish2");
+      chats.renderChat();
+    });//end of logout
 
-    // $('#logout').on('click', function(event){
-    //   event.preventDefault();
-    //   chats.clearChat();
-    //   localStorage.removeItem('userId');
-    //
-    // });
   },
   config: {
     url:'http://tiy-fee-rest.herokuapp.com/collections/chatInTheBox1',
@@ -62,6 +52,18 @@ var chats = {
     $el.append(template);
   },
   renderChat: function(){
+    var login = localStorage.userId;
+    if (login == null) {
+      $('.chatbox').removeClass('show');
+      $('.create').removeClass('show');
+      $('.logout').removeClass('show');
+    } else {
+      $('.chatbox').addClass('show');
+      $('.create').addClass('show');
+      $('.userId').removeClass('show');
+
+    }
+    console.log( login);
     $.ajax({
       url: chats.config.url,
       type: 'GET',
@@ -77,7 +79,6 @@ var chats = {
         console.log(err, "render error");
       }
     });//end ajax for render
-    //set timeout to auto refresh page
   },
   createMessage: function(message) {
     $.ajax({
@@ -94,18 +95,20 @@ var chats = {
   });//end createMessage ajax
 
 },
-deleteMessage: function(id) {
+editName: function (id, name) {
   $.ajax({
-      url: tasks.config.url + '/' + id,
-      type: 'DELETE', //D is for Delete in CRUD
-      success: function (data) {
-        tasks.renderTasks();
-      },
-      error: function(err) {
-        console.log(err , "deleteMessage error"); //You DONE BAD!
-      }
-    }); //end ajax for delete
+    userName: userId,
+    data: message,
+    type: 'PUT',
+    success: function() {
+
   },
+    error: function(err){
+
+    }
+  });
+
+}
 
 
 };//end chats methods
